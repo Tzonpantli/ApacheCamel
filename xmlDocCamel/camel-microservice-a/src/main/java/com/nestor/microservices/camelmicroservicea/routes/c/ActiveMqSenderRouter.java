f3b1a886a31f163373ab1e5123622304f8b3e62e
Tzonpantli <86860824+Tzonpantli@users.noreply.github.com>
2022-10-12 13:17:54 -0500

    Add files via upload

---
+++ xmlDocCamel/camel-microservice-a/src/main/java/com/nestor/microservices/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
@@ -0,0 +1,25 @@
+package com.nestor.microservices.camelmicroservicea.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+    @Override
+    public void configure() throws  Exception{
+        //timer
+ //       from("timer:active-mq-timer?period=10000")
+   //             .transform().constant("My meessage for active MQ")
+     //           .log("${body}")
+       //         .to("activemq:my.queue");
+        //queue
+//        from("file:files/json")
+//                //.transform().constant("My meessage for active MQ")
+ //               .log("${body}")
+ //               .to("activemq:my-activemq-queue");
+        from("file:files/xml")
+                .log("${body}")
+                .to("activemq:my-activemq-xml-queue");
+    }
+
+}
