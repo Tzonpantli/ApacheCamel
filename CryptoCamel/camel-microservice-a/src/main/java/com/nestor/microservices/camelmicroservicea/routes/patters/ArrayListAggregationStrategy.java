b8b7eb92a4d9014797a77a34e50b0fe8c75eb3c8
Tzonpantli <86860824+Tzonpantli@users.noreply.github.com>
2022-10-25 12:36:26 -0500

    Add files via upload

---
+++ CryptoCamel/camel-microservice-a/src/main/java/com/nestor/microservices/camelmicroservicea/routes/patters/ArrayListAggregationStrategy.java
@@ -0,0 +1,24 @@
+package com.nestor.microservices.camelmicroservicea.routes.patters;
+
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Exchange;
+
+import java.util.ArrayList;
+
+public class ArrayListAggregationStrategy implements AggregationStrategy {
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            Object newBody = newExchange.getIn().getBody();
+            ArrayList<Object> list = null;
+            if (oldExchange == null) {
+                list = new ArrayList<Object>();
+                list.add(newBody);
+                newExchange.getIn().setBody(list);
+                return newExchange;
+            } else {
+                list = oldExchange.getIn().getBody(ArrayList.class);
+                list.add(newBody);
+                return oldExchange;
+            }
+        }
+    }
